f56f8003f4751586d0304fe29b282de115b2db8b
ktmouk <ktmouk@gmail.com>
2024-08-22 23:50:15 +0900

    feat: add the today line (#80)

---
+++ apps/web/src/app/[locale]/app/calendar/_components/CalendarTodayLine.tsx
@@ -0,0 +1,8 @@
+export const CalendarTodayLine = () => {
+  return (
+    <div className="flex items-center">
+      <span className="size-3 absolute -mx-3 rounded-full bg-red-500" />
+      <span className="h-0.5 bg-red-500 w-full" />
+    </div>
+  );
+};
